457bdc1e0bf3b3cff6a30665a4a303e36d213cb0
juyeopkim <71051589+juyeopkim@users.noreply.github.com>
2023-10-20 13:25:14 +0900

    1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int all_files;
+
+extern void sub(void);
+
+int main(int argc, char *argv[])
+{
+  sub();
+  printf("%d\n", all_files);
+   
+  system("PAUSE");	
+  return 0;
+}
+
+
